fbf7e2ce1a6768d4003917bd8f2fe78ab26500cf
hiltsjesmilde-optimizely <hsmilde@ams-hsmilde1.local>
2018-05-04 17:21:47 +0200

    rewrore reset js

---
+++ resetJS.js
@@ -0,0 +1,15 @@
+switch (extension.placement) {
+  case 'before':
+  case 'after':
+  case 'prepend':
+  case 'append':
+    var extensionHtml = document.getElementById('optimizely-extension-' + extension.$instance);
+    if (extensionHtml) extensionHtml.parentElement.removeChild(extensionHtml)
+    break;
+  case 'replace-content':
+    var origHtml = '#optimizely-extension-' + extension.$instance + '-orig'.innerHTML;
+    (extension.selector).innerHTML = origHtml;
+    break;
+  default:
+    throw new Error('Unknown placement ' + extension.placement);
+}
